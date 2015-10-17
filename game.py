3132b73e2945f02c376d91ec910d8cbba32302c9
Diogo Gomes <dgomes@av.it.pt>
2015-10-17 23:31:43 +0100

    support multiple players

--- game.py
+++ game.py
@@ -14,7 +14,6 @@ BET_MULTIPLIER = 2
 class Game(object):
     class PlayerState():
         def __init__(self, p):
-            print(chr(27) + "[2J")
             self.player = p
             self.bet = 0
             self.hand = []
@@ -32,39 +31,53 @@ class Game(object):
             h.hand = h.hand[1:]
             return h 
 
-    def __init__(self, players, shoe_size=4):
+    def __init__(self, players, shoe_size=4, debug=False):
+#       print(chr(27) + "[2J")
+        print("-"*80)
+        self.debug = debug
         self.shoe = card.Shoe(shoe_size)
         self.shoe.shuffle()
         self.state = [self.PlayerState(Dealer())] + [self.PlayerState(p) for p in players]
         
         self.state[0].hand += self.shoe.deal_cards(2)
-        self.state[1].hand += self.shoe.deal_cards(2)
+        for p in self.state[1:]:
+            p.hand += self.shoe.deal_cards(2)
         
         self.done = False
 
+    def str_players_hands(self):
+        o = ""
+        for p in self.state[1:]:
+            o+="{:^45}".format(p)
+        return o
+    
+    def str_players_names(self):
+        o = ""
+        for p in self.state[1:]:
+            o+="{:^35}".format(p.player)
+        return o
+
     def __str__(self):
         return (\
-        "{:^"+str(30)+"}\n"\
-        "╔═══════════════════════════════╗\n"\
-        "{:^45}\n"\
-        "                         \n"\
-        "                         \n"\
-        "                         \n"\
-        "                         \n"\
-        "                         \n"\
-        "{:^45}\n"\
-        "╚═══════════════════════════════╝\n"\
         "{:^30}\n"\
-        ).format(self.state[0].player.name, self.state[0].hand if self.done else ["**"]+self.state[0].hide_card().hand, self.state[1], self.state[1].player) + \
-        "\nDealer: {}\n"\
-        "Player: {}\n".format(card.value(self.state[0].hand), card.value(self.state[1].hand))
-
+        "╔"+"═══════════════════════════════"*(len(self.state)-1)+"╗\n"\
+        "{:^45}\n"\
+        "                         \n"\
+        "                         \n"\
+        "                         \n"\
+        "                         \n"\
+        "                         \n"\
+        "{}\n"\
+        "╚"+"═══════════════════════════════"*(len(self.state)-1)+"╝\n"\
+        "{}\n"\
+        ).format(self.state[0].player.name, self.state[0].hand if self.done else ["**"]+self.state[0].hide_card().hand, self.str_players_hands(), self.str_players_names()) 
 
     def deal(self, num):
         return self.shoe.deal_cards(1)
 
     def take_bets(self):
-        print(self)
+        if self.debug: 
+            print(self) 
         for p in self.state[1:]:
             bet = 0
             while bet <=0:
@@ -79,8 +92,9 @@ class Game(object):
                 if p.bust:  #skip bust players
                     continue    
 
-                print("TURN: " + p.player.name)
-                print(self)
+                if self.debug:
+                    print("TURN: " + p.player.name)
+                    print(self)
                 action = ""
                 while action not in ["h", "s"]:
                     if isinstance(p.player, Dealer):
@@ -92,14 +106,11 @@ class Game(object):
                     p.hand+=self.deal(1)
                     hits +=1
                 
-                if card.value(p.hand) > 21:
-                    p.bust = True    
+                if card.value(p.hand) >= 21:
+                    if card.value(p.hand) > 21:
+                        p.bust = True
                     if isinstance(p.player, Dealer):
                         self.done = True
-                        break
-                if card.value(p.hand) == 21:
-                    self.done = True
-                    break
 
         self.done = True
         return [p for p in self.state if 
@@ -111,11 +122,11 @@ class Game(object):
     def payback(self, winners):
         for p in self.state[1:]:
             if p in winners and card.value(self.state[0].hand) == card.value(p.hand):
-                p.player.payback(p.bet)  #bet is returned
+                p.player.payback(0)  #bet is returned
             elif p in winners:
-                p.player.payback(p.bet*BET_MULTIPLIER)
+                p.player.payback(-p.bet + p.bet*BET_MULTIPLIER)
             else:
-                p.player.payback(0) #this means the player lost
+                p.player.payback(-p.bet) #this means the player lost
 
     def run(self):
         self.take_bets()

a69aef861852e5eff6e4c2ade2548f8518ac3fe7
Diogo Gomes <dgomes@av.it.pt>
2015-11-26 00:16:51 +0000

    testplayer moved into each test

--- tests/test3.py
+++ tests/test3.py
@@ -3,9 +3,19 @@ import sys
 sys.path.insert(0,"..")
 from game import Game
 from player import Player
-from testplayer import TestPlayer
 from card import Card
 from test_shoe import TestShoe
+
+class TestPlayer(Player):
+    def __init__(self, name="TestPlayer", money=0, default_bet=1):
+        super(TestPlayer, self).__init__(name, money)
+        self.default_bet = default_bet
+
+    def play(self, dealer, players):
+        return "h"
+
+    def bet(self, dealer, players):
+        return self.default_bet 
 
 if __name__ == '__main__':
 
